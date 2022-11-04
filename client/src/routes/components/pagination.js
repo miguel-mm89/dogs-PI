b6551f71192278cea0545e09e319bdfc82d65733
miguel-mm89 <miguelm.developer@outlook.com>
2022-11-04 02:10:47 -0300

    make it better

--- client/src/routes/components/pagination.js
+++ client/src/routes/components/pagination.js
@@ -4,14 +4,10 @@ import { GrNext, GrPrevious } from "react-icons/gr";
 import { useDispatch, useSelector } from "react-redux";
 import { paginate } from "../../redux/actions";
 
-export default function Pagination({
-  cardPerPage,
-  totalCards,
-}) {
+export default function Pagination({ cardPerPage, totalCards }) {
+  const currentPage = useSelector((state) => state.currentPage);
+  const dispatch = useDispatch();
 
-  const currentPage = useSelector(state => state.currentPage)
-  const dispatch = useDispatch()
-  
   if (Math.ceil(totalCards / cardPerPage) < currentPage) {
     dispatch(paginate(1));
   }
@@ -22,37 +18,46 @@ export default function Pagination({
   }
 
   let pageShow = pageNumbers.slice(0, 5);
-  if(pageNumbers.length > 5 && currentPage > 3){
-     pageShow = pageNumbers.slice(currentPage -3, currentPage +2)
-     
+  if (pageNumbers.length > 5 && currentPage > 3) {
+    pageShow = pageNumbers.slice(currentPage - 3, currentPage + 2);
   }
-  if(currentPage === pageNumbers.length -1 && currentPage > 4){
-    pageShow = pageNumbers.slice(currentPage -4, currentPage+2)
-   
- }
- if(currentPage === pageNumbers.length && currentPage+1 > 5){
-  pageShow = pageNumbers.slice(currentPage -5, currentPage+2)
- 
-}
-  
+  if (currentPage === pageNumbers.length - 1 && currentPage > 4) {
+    pageShow = pageNumbers.slice(currentPage - 4, currentPage + 2);
+  }
+  if (currentPage === pageNumbers.length && currentPage + 1 > 5) {
+    pageShow = pageNumbers.slice(currentPage - 5, currentPage + 2);
+  }
 
   return (
     <>
-    <PaginationContainer>
-      <NextPrev>
-      <button disabled={currentPage === 1? true: false} className={currentPage === 1? 'disable': null} onClick={() => dispatch(paginate(currentPage -1))}><GrPrevious/></button>
-      </NextPrev>
-      <NumbersDiv>
-      {pageShow?.map(item => <div className={currentPage === item? "active": null} key={item}>
-        <button  onClick={() => dispatch(paginate(item)) }>{item}</button>
-      </div> 
-      )}
-      </NumbersDiv>
-      <NextPrev>
-      <button className={currentPage === pageNumbers.length? 'disable': null} disabled={currentPage === pageNumbers.length? true: false} onClick={() => dispatch(paginate(currentPage +1))}><GrNext/></button>
-      </NextPrev>
-    </PaginationContainer>
-</>
+      <PaginationContainer>
+        <NextPrev>
+          <button
+            disabled={currentPage === 1 ? true : false}
+            className={currentPage === 1 ? "disable" : null}
+            onClick={() => dispatch(paginate(currentPage - 1))}
+          >
+            <GrPrevious />
+          </button>
+        </NextPrev>
+        <NumbersDiv>
+          {pageShow?.map((item) => (
+            <div className={currentPage === item ? "active" : null} key={item}>
+              <button onClick={() => dispatch(paginate(item))}>{item}</button>
+            </div>
+          ))}
+        </NumbersDiv>
+        <NextPrev>
+          <button
+            className={currentPage === pageNumbers.length ? "disable" : null}
+            disabled={currentPage === pageNumbers.length ? true : false}
+            onClick={() => dispatch(paginate(currentPage + 1))}
+          >
+            <GrNext />
+          </button>
+        </NextPrev>
+      </PaginationContainer>
+    </>
   );
 }
 
@@ -72,7 +77,7 @@ const PaginationContainer = styled.div`
   align-items: center;
   height: 34px;
   overflow: hidden;
-  box-shadow: 4px 3px rgb(0,0,0,.3);
+  box-shadow: 4px 3px rgb(0, 0, 0, 0.3);
 `;
 
 const NumbersDiv = styled.div`
@@ -93,19 +98,18 @@ const NumbersDiv = styled.div`
     flex-direction: row;
     /* margin-right: 5px; */
   }
-  .active{
+  .active {
     background-color: rgb(129, 50, 54);
     height: 90%;
     padding: 5px 0;
     display: flex;
     align-items: center;
-    button{
+    button {
       padding: 10px;
       color: whitesmoke;
       margin: 3px;
     }
   }
-
 `;
 
 const NextPrev = styled.div`
@@ -116,12 +120,12 @@ const NextPrev = styled.div`
   height: 25px;
   margin: 7px;
   border-radius: 25%;
-  
-  .disable{
-   display: none;
-   /* color: aquamarine; */
+
+  .disable {
+    display: none;
+    /* color: aquamarine; */
   }
-  
+
   button {
     background: none;
     cursor: pointer;
