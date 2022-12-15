d5b15bea9edf1080ab09e55cbfc44e71494fee83
miguel-mm89 <miguelm.developer@outlook.com>
2022-12-15 00:16:20 -0300

    deploy

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,9 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-
-import dotenv from 'dotenv'
+// import dotenv from 'dotenv'
 import axios from "axios";
-dotenv.config()
+// dotenv.config()
 
 axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001'
 
